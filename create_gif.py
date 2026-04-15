e7a9aad3e5398507c2d3621e83134d51c7b69f29
Cross Leonard <cleonardross11@gmail.com>
2026-04-15 14:14:47 +0100

    fixed issue with different sized images

--- create_gif.py
+++ create_gif.py
@@ -1,9 +1,23 @@
 import imageio.v3 as iio
+from PIL import Image
+import numpy as np
 
-filenames = ['nyan-cat1.png', 'nyan-cat2.png', 'nyan-cat3.png']
-images = [ ]
+#insert image filenames
+#         v
+pic1 = 'shitphys.JPG'
+pic2 = 'p1.jpg' 
+pic3 = 'p2.jpg' 
+pic4 = 'p3.jpg'
+pic5 = 'p4.jpg'
+pic6 = 'goatphys.jpg'
+
+filenames = [pic1, pic2, pic3, pic4, pic5, pic6]
+images = []
+
+first = Image.open(filenames[0])
+target_size = first.size
 
 for filename in filenames:
-  images.append(iio.imread(filename))
+  images.append(np.array(Image.open(filename).resize(target_size)))
 
-iio.imwrite('nyan.gif', images, duration = 500, loop = 0)
+iio.imwrite('nyan.gif', images, duration = 1000, loop = 0)
